474ed36944d8539a558e1432237d831194e38cea
goosey-goose <g.ebenezer.miranda@gmail.com>
2021-09-12 15:28:03 -0700

    ADD REMAINING CLICKABLE DIV LINKS

--- assets/js/clickables.js
+++ assets/js/clickables.js
@@ -1,8 +1,26 @@
-// 658
 let watchItDiv = document.getElementById("watch-it");
 
 watchItDiv.addEventListener('click', () => {
-    // console.log("they are watching");
     // window.location.href = "https://www.google.com";
     window.open('https://watch-it-app.herokuapp.com/login', '_blank').focus();
+})
+
+
+/////////////////////////////////////////////////////////////////////////////////////
+
+
+let ketchupDiv = document.getElementById("ketchup");
+
+ketchupDiv.addEventListener('click', () => {
+    window.open('https://aa-ketchup.herokuapp.com/', '_blank').focus();
+})
+
+
+/////////////////////////////////////////////////////////////////////////////////////
+
+
+let pbrainDiv = document.getElementById("pbrain");
+
+pbrainDiv.addEventListener('click', () => {
+    window.open('https://pbrain-app.herokuapp.com/', '_blank').focus();
 })
