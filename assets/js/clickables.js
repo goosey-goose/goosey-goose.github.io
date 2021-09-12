ce784c21db921c30a0122f89cc26c9f12c20267f
goosey-goose <g.ebenezer.miranda@gmail.com>
2021-09-12 14:59:28 -0700

    test

--- assets/js/clickables.js
+++ assets/js/clickables.js
@@ -3,5 +3,5 @@ let watchItDiv = document.getElementById("watch-it");
 
 watchItDiv.addEventListener('click', () => {
     // console.log("they are watching");
-    location.href("https://www.google.com");
+    window.location.href("https://www.google.com");
 })
