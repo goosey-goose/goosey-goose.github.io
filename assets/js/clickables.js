2b76630af7bb65ebc3735f7615a73d43d6843c3b
goosey-goose <g.ebenezer.miranda@gmail.com>
2021-11-16 16:35:40 -0800

    UPDATE RESUME LINK

--- assets/js/clickables.js
+++ assets/js/clickables.js
@@ -47,5 +47,5 @@ let resumeSpan = document.getElementById("resume_span");
 
 resumeSpan.addEventListener('click', () => {
     // console.log("resume clicked");
-    window.open('https://drive.google.com/file/d/1iZNkCjZu5d8O8jTvsQcji6sL0_ZXwoky/view?usp=sharing', '_blank').focus();
+    window.open('https://drive.google.com/file/d/1s7nEILY7Coo06vDiktUKULVGfe-p6bxz/view?usp=sharing', '_blank').focus();
 })
