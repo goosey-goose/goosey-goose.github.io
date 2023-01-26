ed1c07c83860a24efaf119ce7d2518e5807d822c
Gustavo Miranda <g.ebenezer.miranda@gmail.com>
2023-01-26 03:27:41 +0000

    CONTINUE DEBUGGING BROKEN RESUME LINK

--- assets/js/clickables.js
+++ assets/js/clickables.js
@@ -45,6 +45,9 @@ medievalBNBDiv.addEventListener('click', () => {
 
 let resumeSpan = document.getElementById("resume_span");
 
+//  DELETE
+console.log(resumeSpan);
+
 resumeSpan.addEventListener('click', () => {
     console.log("resume clicked");
     window.open('https://drive.google.com/file/d/1s7nEILY7Coo06vDiktUKULVGfe-p6bxz/view?usp=sharing', '_blank').focus();
